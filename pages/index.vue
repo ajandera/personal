c351e043849cdb9b82bc6f767deb0ded8f41edbc
Ales Jandera <ales.jandera@gmail.com>
2024-02-11 19:20:37 +0100

    Update podcast cover.

--- pages/index.vue
+++ pages/index.vue
@@ -43,56 +43,6 @@
           <p>Develope since 7/2021 after success in a <strong>Startup Incubator in Technicom</strong>, part of TUKE university in Košice.</p>
         </div>
       </div>
-      <div class="row" id="shopycrm">
-        <div class="col-12">
-          <div class="divider"></div>
-        </div>
-      </div>
-      <div class="row mt-5">
-        <div class="col-md-8 col-sm-12 col-xs-12 text-justify">
-          <h2>Elevate Your Sales Game with ShopyCRM!</h2>
-          <p>Ready to supercharge your <strong>sales process</strong> and take your business to new heights? Look no further than ShopyCRM – your all-in-one solution for seamless sales management and automation!</p>
-          <p>Since 2014, we've been leading the charge in revolutionizing CRM technology. Our SaaS CRM application isn't just another run-of-the-mill platform – it's a powerhouse of innovation tailored specifically for sales companies like yours.</p>
-          <p>What sets ShopyCRM apart? Our focus on automation and seamless integration with third-party applications is unmatched. Say goodbye to manual data entry and hello to effortless connectivity!</p>
-          <p><strong>With ShopyCRM, you can:</strong></p>
-          <ul>
-            <li>Easily integrate your online store, marketplaces, accounting software, logistics platforms, and warehouse storage systems</li>
-            <li>Streamline communication and data exchange with XML or CSV specifications – no hassle, no fuss!</li>
-            <li>Enjoy custom connections and bespoke solutions tailored to your unique business needs</li>
-          </ul>
-          <p>Ready to experience the ShopyCRM difference for yourself? Visit shopycrm.com and sign up for <strong>FREE</strong> today to unlock the full potential of our cloud-based CRM app.</p>
-          <p>Don't miss out on this game-changing opportunity to revolutionize your sales process and drive unprecedented growth! Join the ShopyCRM family today and unleash the power of seamless sales management.</p>
-        </div>
-        <div class="col-md-4 col-sm-12 col-xs-12">
-          <img :src="images[1]" class="img-fluid" />
-          <img :src="images[2]" class="img-fluid mt-5"/>
-          <a href="https://shopycrm.com" target="_blank" class="btn btn-success btn-lg actionBtn">Register for FREE</a>
-        </div>
-      </div>
-      <div class="row" id="underthelid">
-        <div class="col-12">
-          <div class="divider"></div>
-        </div>
-      </div>
-      <div class="row mt-5">
-        <div class="col-md-4 col-sm-12 col-xs-12">
-          
-        </div> 
-        <div class="col-md-8 col-sm-12 col-xs-12 text-justify">
-          <h2>Discover the Secrets of Success in Entrepreneurship and AI-Powered E-Commerce!</h2>
-          <p>Embark on a journey behind the scenes of <strong>entrepreneurship and AI innovation</strong> with me, a seasoned senior software developer with over a decade of experience. From navigating international teams to crafting my own CRM software since 2014, my entrepreneurial ventures have led me to develop a groundbreaking startup centered around mathematical modeling in e-commerce.</p>
-          <p>Join me as I unveil the tips, tricks, and real-life experiences that have shaped my entrepreneurial path. Gain invaluable insights into:</p>
-          <ul>
-            <li>Navigating the intricacies of international collaboration and team dynamics</li>
-            <li>Unveiling the secrets behind the creation and evolution of my own CRM software</li>
-            <li>Exploring the transformative potential of AI in e-commerce through mathematical modeling</li>
-            <li>Harnessing AI for human behavior prediction and revolutionizing customer engagement</li>
-          </ul>
-          <p>Through candid discussions and firsthand accounts, you'll uncover the regular days and extraordinary moments that define the journey of entrepreneurship and AI innovation.</p>
-          <p>Ready to unlock the keys to success and propel your own ventures forward? Join me for an enlightening exploration of the intersection between technology, entrepreneurship, and human behavior prediction.</p>
-          <p>Sign up now for an exclusive insider look at the world of entrepreneurship and AI-powered e-commerce!</p>
-        </div>
-      </div>
       <div class="row" id="consultaitons">
         <div class="col-12">
           <div class="divider"></div>
@@ -111,6 +61,56 @@
           <script type="text/javascript" src="https://assets.calendly.com/assets/external/widget.js" async></script>
           <!-- Calendly inline widget end -->
         </div>
+      </div>
+      <div class="row" id="shopycrm">
+        <div class="col-12">
+          <div class="divider"></div>
+        </div>
+      </div>
+      <div class="row mt-5">
+        <div class="col-md-4 col-sm-12 col-xs-12">
+          <img :src="images[1]" class="img-fluid" />
+          <img :src="images[2]" class="img-fluid mt-5"/>
+          <a href="https://shopycrm.com" target="_blank" class="btn btn-success btn-lg actionBtn">Register for FREE</a>
+        </div>
+        <div class="col-md-8 col-sm-12 col-xs-12 text-justify">
+          <h2>Elevate Your Sales Game with ShopyCRM!</h2>
+          <p>Ready to supercharge your <strong>sales process</strong> and take your business to new heights? Look no further than ShopyCRM – your all-in-one solution for seamless sales management and automation!</p>
+          <p>Since 2014, we've been leading the charge in revolutionizing CRM technology. Our SaaS CRM application isn't just another run-of-the-mill platform – it's a powerhouse of innovation tailored specifically for sales companies like yours.</p>
+          <p>What sets ShopyCRM apart? Our focus on automation and seamless integration with third-party applications is unmatched. Say goodbye to manual data entry and hello to effortless connectivity!</p>
+          <p><strong>With ShopyCRM, you can:</strong></p>
+          <ul>
+            <li>Easily integrate your online store, marketplaces, accounting software, logistics platforms, and warehouse storage systems</li>
+            <li>Streamline communication and data exchange with XML or CSV specifications – no hassle, no fuss!</li>
+            <li>Enjoy custom connections and bespoke solutions tailored to your unique business needs</li>
+          </ul>
+          <p>Ready to experience the ShopyCRM difference for yourself? Visit shopycrm.com and sign up for <strong>FREE</strong> today to unlock the full potential of our cloud-based CRM app.</p>
+          <p>Don't miss out on this game-changing opportunity to revolutionize your sales process and drive unprecedented growth! Join the ShopyCRM family today and unleash the power of seamless sales management.</p>
+        </div>
+      </div>
+      <div class="row" id="underthelid">
+        <div class="col-12">
+          <div class="divider"></div>
+        </div>
+      </div>
+      <div class="row mt-5">
+        <div class="col-md-8 col-sm-12 col-xs-12 text-justify">
+          <h2>Discover the Secrets of Success in Entrepreneurship and AI-Powered E-Commerce!</h2>
+          <p>Embark on a journey behind the scenes of <strong>entrepreneurship and AI innovation</strong> with me, a seasoned senior software developer with over a decade of experience. From navigating international teams to crafting my own CRM software since 2014, my entrepreneurial ventures have led me to develop a groundbreaking startup centered around mathematical modeling in e-commerce.</p>
+          <p>Join me as I unveil the tips, tricks, and real-life experiences that have shaped my entrepreneurial path. Gain invaluable insights into:</p>
+          <ul>
+            <li>Navigating the intricacies of international collaboration and team dynamics</li>
+            <li>Unveiling the secrets behind the creation and evolution of my own CRM software</li>
+            <li>Exploring the transformative potential of AI in e-commerce through mathematical modeling</li>
+            <li>Harnessing AI for human behavior prediction and revolutionizing customer engagement</li>
+          </ul>
+          <p>Through candid discussions and firsthand accounts, you'll uncover the regular days and extraordinary moments that define the journey of entrepreneurship and AI innovation.</p>
+          <p>Ready to unlock the keys to success and propel your own ventures forward? Join me for an enlightening exploration of the intersection between technology, entrepreneurship, and human behavior prediction.</p>
+          <p>Sign up now for an exclusive insider look at the world of entrepreneurship and AI-powered e-commerce!</p>
+        </div>
+        <div class="col-md-4 col-sm-12 col-xs-12">
+          
+        </div> 
       </div>
     </div>
   </section>
